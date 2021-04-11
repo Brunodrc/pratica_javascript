9d91b18bdd861ccb866afd6e10480b2002caaa0f
BRUNO DIEGO RESENDE CASTRO <50345783+Brunodrc@users.noreply.github.com>
2021-04-11 10:16:59 -0300

    Delete conversor_dec_bin.js

--- conversor_dec_bin.js
+++
@@ -1,9 +0,0 @@
-let entrada = 27
-let total = []
-while (entrada >= 1) {
-    resto = entrada % 2
-    div_interia = Number.parseInt(entrada / 2)
-    total.push(resto)
-    entrada = div_interia
-}
-console.log(total.reverse())
